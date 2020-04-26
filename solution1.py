9fd368f4b7380539ed667a0a119b875c84f98db8
Yaad Nahshon <ydnahshon@gmail.com>
2020-04-26 22:23:09 +0300

    Initial files.

---
+++ solution1.py
@@ -0,0 +1,19 @@
+
+MIN_VALUE = 97
+MAX_VALUE = 122
+
+
+def solution1(x):
+    output = ""
+    for ch in x:
+        ch_decimal = ord(ch)
+        if MIN_VALUE <= ch_decimal <= MAX_VALUE:
+            output = output + chr(MAX_VALUE - (ch_decimal - MIN_VALUE))
+        else:
+            output = output + ch
+    print(output)
+
+
+if __name__ == '__main__':
+    test1 = "wrw blf hvv ozhg mrtsg'h vkrhlwv?"
+    solution1(test1)
